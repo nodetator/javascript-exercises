9a46489ca1f00ebbdda637ab4ad9cd9fa319d6df
nodetator <nodetator@gmail.com>
2022-11-11 10:57:46 +0800

    Passed 2nd test with ugly code!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(x, y) {
+    
+    let finalString = x;
 
-};
+    if (y < 0) {
+        finalString = 'ERROR';
+    } else if (y == 0) {
+        finalString = '';
+    } else {
+    for (let i = 1; i < y; i++) {
+      finalString = finalString + x;
+    } 
+    }
+    return finalString;
+  };
+
+  //above code fails if 
+
+//repeatString('hey', 5);
 
 // Do not edit below this line
 module.exports = repeatString;
