bddcbb3be4322b7d2dac323371a92f939b49c9ee
nodetator <nodetator@gmail.com>
2022-11-11 21:46:24 +0800

    I peeked on the solutions on the 4th exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(character) {
+    character = character.split('');
+    character = character.reverse();
+    let reverseString = character.join("");
+    return reverseString;
 };
 
+reverseString("a string here");
 // Do not edit below this line
 module.exports = reverseString;
