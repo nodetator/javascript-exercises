1dd8ae25f158f8e9cd8c9bdf94a04277baed1375
nodetator <nodetator@gmail.com>
2022-11-12 01:28:00 +0800

    Finished javacsript exercisesgit add .!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahren) {
+  celsius = (fahren - 32) * 0.5556;
+  celsius = Math.round(celsius * 10) / 10;
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  fahren = (celsius * 1.8) + 32;
+  fahren = Math.round(fahren * 10) / 10
+  return fahren
 };
 
 // Do not edit below this line
